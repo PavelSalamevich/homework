7d9b522d11d5ce6b09dacdcf8cd9b4acca174c3f
PavelSalamevich <132605754+PavelSalamevich@users.noreply.github.com>
2023-05-31 22:44:30 +0200

    Add files via upload

---
+++ HomeWorkChar3Examles.java
@@ -0,0 +1,26 @@
+package HomeWork;
+
+public class HomeWorkChar3Examles {
+    public static void main(String[] args) {
+        char q = '0';
+        char w = '0';
+        char e = '0';
+
+        int qq = q;
+        System.out.println(qq);
+        System.out.println();
+        System.out.println((int)'0');
+        System.out.println();
+
+        String ww = String.valueOf(w);
+        int www = Integer.parseInt(ww);
+        System.out.println(www);
+
+        System.out.println();
+        int ee = Character.getNumericValue(e);
+        System.out.println(ee);
+
+
+
+    }
+}
